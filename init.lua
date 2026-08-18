8e8794fc880fd6ffb84b46037e3db870d23695d4
Peter Vu <dmtruong1980@gmail.com>
2026-08-18 01:38:11 -0500

    chore: Enable transparent background

--- init.lua
+++ init.lua
@@ -385,6 +385,7 @@ do
   vim.pack.add { gh 'folke/tokyonight.nvim' }
   ---@diagnostic disable-next-line: missing-fields
   require('tokyonight').setup {
+    transparent = true,
     styles = {
       comments = { italic = false }, -- Disable italics in comments
     },
